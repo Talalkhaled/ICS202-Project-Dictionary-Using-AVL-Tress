022c536a4aea1315ecb1689a1c8097b438013091
Talal Al-Harbi <85645218+Talalkhaled@users.noreply.github.com>
2023-06-11 17:29:19 +0300

    Update AVLTree.java

--- src/AVLTree.java
+++ src/AVLTree.java
@@ -413,9 +413,5 @@ public class AVLTree<T extends Comparable<T>> {
         this.rotateLeft();
         this.adjustHeight();
 
-<<<<<<< HEAD
-=======
-}
->>>>>>> 35aa03436fdf1e1ca26f8fac0dfabbd577ac69d7
 }
 }
